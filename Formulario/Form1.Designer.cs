8f69e02a059ac62ba4e881363ce1b8f23cddb293
karla2206 <88003228+karla2206@users.noreply.github.com>
2021-07-29 21:10:20 -0600

    Add files via upload

---
+++ Formulario/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+using System;
+
+namespace Formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttclickaqui = new System.Windows.Forms.Button();
+            this.lblintegrantes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttclickaqui
+            // 
+            this.bttclickaqui.BackColor = System.Drawing.SystemColors.Control;
+            this.bttclickaqui.Location = new System.Drawing.Point(147, 85);
+            this.bttclickaqui.Name = "bttclickaqui";
+            this.bttclickaqui.Size = new System.Drawing.Size(150, 48);
+            this.bttclickaqui.TabIndex = 0;
+            this.bttclickaqui.Text = "Click aqui";
+            this.bttclickaqui.UseVisualStyleBackColor = false;
+            this.bttclickaqui.Click += new System.EventHandler(this.Bttclickaqui_Click);
+            // 
+            // lblintegrantes
+            // 
+            this.lblintegrantes.AutoSize = true;
+            this.lblintegrantes.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.lblintegrantes.Location = new System.Drawing.Point(169, 152);
+            this.lblintegrantes.Name = "lblintegrantes";
+            this.lblintegrantes.Size = new System.Drawing.Size(103, 16);
+            this.lblintegrantes.TabIndex = 1;
+            this.lblintegrantes.Text = "INTEGRANTES";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.label1.Location = new System.Drawing.Point(116, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "-";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.label2.Location = new System.Drawing.Point(116, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.label3.Location = new System.Drawing.Point(116, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.label4.Location = new System.Drawing.Point(116, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.label5.Location = new System.Drawing.Point(116, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Script", 12F);
+            this.label6.Location = new System.Drawing.Point(33, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(405, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "FORMULARIO PARA LISTADO DE ALUMNOS ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 366);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblintegrantes);
+            this.Controls.Add(this.bttclickaqui);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttclickaqui;
+        private System.Windows.Forms.Label lblintegrantes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Formulario/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bttclickaqui_Click(object sender, EventArgs e)
+        {
+            lblintegrantes.Text = "Grupo 5";
+            label1.Text = " - Jose Alexander Salinas Lobos USIS019021";
+            label2.Text = " - Juan Alfredo Martinez Molina USIS001821";
+            label3.Text = " - Karla Beatriz Castellanos Serpas USIS000621";
+            label4.Text = " - Katherin Yeferin Campos Rodriguez USIS016021";
+            label5.Text = " - Juan Carlos Mejia Morales USIS002221"; 
+
+
+        }
+        }
+    }
